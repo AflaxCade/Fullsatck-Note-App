975cb633f5a08299a59a391e2b59d6f8061a5374
AflaxCade <aflah.cade@gmail.com>
2025-04-27 08:38:13 +0300

    created note model, migrated it, added to the admin page and created super user

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Note
 
 # Register your models here.
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'body', 'created', 'updated')
+    search_fields = ('body',)
+    list_filter = ('created', 'updated')
+    ordering = ('-created',)
+    list_per_page = 10
+    list_display_links = ('id', 'body')
+
